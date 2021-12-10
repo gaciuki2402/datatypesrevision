f36972526dafe351d7807b2b6a5e0eb806fe1fcc
gaciuki2402 <delbrivigrace@gmail.com>
2021-12-10 22:22:33 +0300

    Initial commit

---
+++ ifstatement2.py
@@ -0,0 +1,64 @@
+
+num1=456
+num2=456
+if num1>num2:
+    print(True)
+else:
+    print(False)
+
+    if num1==num2:
+        print(True)
+    else:
+        print(False)
+
+    if num1<num2:
+            print(False)
+    else:
+            print(True)
+
+
+busy=False
+if busy:
+    print("am doing my assignment.")
+else:
+    print("am not doing my assignment.")
+
+iamtired=False
+if iamtired:
+    print("i will sleep.")
+else:
+    print("i will watch the show.")
+
+read=False
+if read:
+    print("bluetick")
+else:
+    print("greytick")
+
+sick=False
+if sick:
+    print("Malaria")
+else:
+    print("just a fever")
+
+day=True
+S1="On sunday"
+S2=", i will go to church"
+S3=" i will not go to church"
+if day:
+    print(S1+S2)
+else:
+    print(S3)
+
+grade=67
+if grade>0 and grade<=30:
+    print("terribly failed")
+elif grade>30 and grade<=45:
+    print("fail")
+elif grade>45 and grade<60:
+    print("fair")
+elif grade>=60 and grade<=80:
+    print("pass")
+else:
+    print("invalid grade")
+
