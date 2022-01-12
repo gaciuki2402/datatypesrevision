846cb9c77412f623e12462e69bb5c4df3b3790b9
gaciuki2402 <delbrivigrace@gmail.com>
2022-01-12 15:02:43 +0300

    Grading

---
+++ morepractice.py
@@ -0,0 +1,25 @@
+score=4
+if score>=85 and score<=100:
+    print("Grade:A")
+elif score>=77 and score<=84:
+    print("Grade:A-")
+elif score>=69 and score<77:
+    print("Grade:B+")
+elif score>=65 and score<69:
+    print("Grade:B")
+elif score>=59 and score<65:
+    print("Grade:B-")
+elif score>=52 and score<59:
+    print("Grade:C+")
+elif score>=43 and score<52:
+    print("Grade:C")
+elif score>=37 and score<43:
+    print("Grade:C-")
+elif score>=30 and score<37:
+    print("Grade:D+")
+elif score>=25 and score<30:
+    print("Grade:D")
+elif score>=19 and score<25:
+    print("Grade:D-")
+elif score<19:
+    print("Grade:E")
